83403e3e08138e16e18090ae7569b8b30d9a1a36
danielvanc <email@danielvanc.com>
2021-03-01 15:24:56 +0000

    Exercise 2 - Extra credit 1 - complete!

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -28,7 +28,7 @@ function asyncReducer(state, action) {
   }
 }
 
-function useAsync(asyncCallback, initialState, dependencies) {
+function useAsync(asyncCallback, initialState) {
   const [state, dispatch] = React.useReducer(asyncReducer, {
     status: 'idle',
     data: null,
@@ -52,24 +52,33 @@ function useAsync(asyncCallback, initialState, dependencies) {
         dispatch({type: 'rejected', error})
       },
     )
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, dependencies)
+  }, [asyncCallback])
 
   return state
 }
 
 function PokemonInfo({pokemonName}) {
 
-  const state = useAsync(
-    () => {
-      if (!pokemonName) {
-        return
-      }
-      return fetchPokemon(pokemonName)
-    },
-    {status: pokemonName ? 'pending' : 'idle'},
-    [pokemonName],
-  )
+  const asyncCallback = React.useCallback(() => {
+    if (!pokemonName) return
+
+    return fetchPokemon(pokemonName)
+  }, [pokemonName])
+  
+  const state = useAsync(asyncCallback, {
+    status: pokemonName ? 'pending' : 'idle',
+  })
+
+  // const state = useAsync(
+  //   () => {
+  //     if (!pokemonName) {
+  //       return
+  //     }
+  //     return fetchPokemon(pokemonName)
+  //   },
+  //   {status: pokemonName ? 'pending' : 'idle'},
+  //   [pokemonName],
+  // )
   
   const {data, status, error} = state
 
