ea8c79ff5ef48b16b5cc9bd8cc50864ad6a47fdd
danielvanc <email@danielvanc.com>
2021-03-02 17:30:18 +0000

    Exercise 3 - Extra credit 1 - complete!

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -2,23 +2,38 @@
 // http://localhost:3000/isolated/exercise/03.js
 
 import * as React from 'react'
+// You could also import the below extraction to a seperate module and
+// include like so:
+// import { CountProvider, useCount } from '../myCountCountextFile
+
 
 const CountContext = React.createContext()
 
 function CountProvider(props) {
   const [count, setCount] = React.useState(0);
   const value = [count, setCount]
-
+  
   return <CountContext.Provider value={value} {...props} />
 }
 
+function useCount(props) {
+  const context = React.useContext(CountContext)
+  if (!context) {
+    throw new Error(`useCount must be used within the CountProvider`)
+  }
+
+  return context
+}
+
 function CountDisplay() {
-  const [count] = React.useContext(CountContext)
+  // const [count] = React.useContext(CountContext)
+  const [count] = useCount()
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  const [,setCount] = React.useContext(CountContext)
+  // const [,setCount] = React.useContext(CountContext)
+  const [, setCount] = useCount()
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
@@ -28,8 +43,8 @@ function App() {
     <div>
       <CountProvider>
         <CountDisplay />
-        <Counter />
       </CountProvider>
+        <Counter />
     </div>
   )
 }
