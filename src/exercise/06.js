cba0581da1c86b65e1dfbb23cdbfde14792bcaba
danielvanc <email@danielvanc.com>
2021-03-03 17:24:53 +0000

    Exercise 6 : Extra credit 1 - complete!

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -6,7 +6,28 @@ import * as React from 'react'
 function useMedia(query, initialState = false) {
   const [state, setState] = React.useState(initialState)
 
-  React.useDebugValue(`\`${query}\` => ${state}`)
+  /**
+   * Example using the 2nd param, custom format function
+   * his is only really useful for situations where computing the 
+   * debug value is computationally expensive (and therefore you only 
+   * want it calculated when the DevTools are open and not when your 
+   * users are using the app).
+  */
+
+//  const formatCountDebugValue = ({initialCount, step}) =>
+//   `init: ${initialCount}; step: ${step}`
+
+//   function useCount({initialCount = 0, step = 1} = {}) {
+//     React.useDebugValue({initialCount, step}, formatCountDebugValue)
+//     const [count, setCount] = React.useState(0)
+//     const increment = () => setCount(c => c + 1)
+//     return [count, increment]
+//   }
+
+
+  React.useDebugValue(
+    `\`${query}\` => ${state}`,
+  )
 
   React.useEffect(() => {
     let mounted = true
